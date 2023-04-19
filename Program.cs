54928d8fabe6d3ac7b6ad5f6c815b5b404d45b30
E_Frolova <katerina199128@mail.ru>
2023-04-19 16:18:18 +0300

    HomeTask3

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+/*
+ Задача 21
+
+ double Div(double X1, double Y1, double Z1, double X2, double Y2, double Z2)
+ {
+    double lenth_X = X2 - X1;
+    double lenth_Y = Y2 - Y1;
+    double lenth_Z = Z2 - Z1;
+    double lenth_XYZ = Math.Sqrt(lenth_X * lenth_X + lenth_Y * lenth_Y + lenth_Z * lenth_Z);
+    return lenth_XYZ;
+ }
+ Console.WriteLine("Введите координату X1");
+ double X1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите координату Y1");
+ double Y1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите координату Z1");
+ double Z1 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите координату X2");
+ double X2 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите координату Y2");
+ double Y2 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите координату Z2");
+ double Z2 = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine(Div(X1, Y1, Z1, X2, Y2, Z2));
+
+
+// Задача 23
+void Cube(int num)
+{
+    for(int i = 1; i <= num; i++) Console.WriteLine($"{i} - {i*i*i}");
+}
+Console.Write("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Cube(num);
+*/
